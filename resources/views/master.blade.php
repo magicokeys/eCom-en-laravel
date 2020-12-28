b9f5b390ffcea6f67c57122255c9be2cca082512
Lester James Tagarda <lesterjamestagarda@gmail.com>
2020-12-28 21:40:58 +0800

    CartList

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -64,6 +64,12 @@
             width: 500px !important;
         }
 
+        .cart-list-divider {
+            border-bottom: 1px solid #ccc;
+            margin-bottom: 20px;
+            padding-bottom: 20px;
+        }
+
     </style>
 
 </html>
