3108760dbdf3ed5aa28105f8996b3d4214c1e75d
Lester James Tagarda <lesterjamestagarda@gmail.com>
2020-12-28 23:37:34 +0800

    Order List & Orders Migration

--- resources/views/myorders.blade.php
+++ resources/views/myorders.blade.php
@@ -5,9 +5,8 @@
         
         <div class="col-sm-10">
             <div class="trending-wrapper">
-                <h4>Result for Products</h4>
-                <a href="ordernow" class="btn btn-success">Order Now</a>
-                @foreach($products as $item)                
+                <h4>My Orders</h4>
+                @foreach($orders as $item)                
                     <div class="row searched-item cart-list-divider">
                         <div class="col-sm-3">
                             <a href="detail/{{ $item->id }}">
@@ -16,12 +15,12 @@
                         </div>
                         <div class="col-sm-4">
                             <div class="">
-                                <h2>{{ $item->name }}</h2>
-                                <h5>{{ $item->description }}</h5>
+                                <h2>Name : {{ $item->name }}</h2>
+                                <h5>Delivery Status : {{ $item->status }}</h5>
+                                <h5>Address : {{ $item->address }}</h5>
+                                <h5>Payment Status : {{ $item->payment_status }}</h5>
+                                <h5>Payment Method : {{ $item->payment_method }}</h5>
                             </div>
-                        </div>
-                        <div class="col-sm-3">
-                            <a href="/removecart/{{ $item->cart_id }}" class="btn btn-warning">Remove from Cart</a>
                         </div>
                     </div>
                 @endforeach
